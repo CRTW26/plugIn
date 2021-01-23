d00a311d6c1fd94e22601825288bdf7caea26fdf
CRTW26 <worthington.connor@gmail.com>
2021-01-23 13:59:36 +0000

    Makes Map statelesss comp

--- README.md
+++ README.md
@@ -20,3 +20,7 @@ npm run test
 ```
 
 For detailed explanation on how things work, checkout the [CLI Readme](https://github.com/developit/preact-cli/blob/master/README.md).
+
+Example query:
+
+https://api.openchargemap.io/v3/poi/?output=json&countrycode=GB&maxresults=10&compact=true&verbose=false&latitude=51.507351&longitude=-0.127758&distance=10&distanceunit=km
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,24 +1,19 @@
-import {h, Component} from 'preact';
+import { h } from 'preact';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
-class Map extends Component {
+const map = () => (
+  <MapContainer
+  center={[51.505, -0.09]}
+  zoom={13}
+  style={{width: '70%', height: '500px'}}
+  >
+  <TileLayer 
+    attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />  
+  </MapContainer>
+);
 
-  render () {
-    return (
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        style={{width: '70%', height: '500px'}}
-      >
-      <TileLayer 
-        attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />  
-      </MapContainer>
-    );
-  }
-}
-
-export default Map;
+export default map;

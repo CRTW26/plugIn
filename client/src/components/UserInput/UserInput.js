0908d36b69401aa979fb62a70b9f503c2d9f9a24
CRTW26 <worthington.connor@gmail.com>
2021-01-27 14:41:01 +0000

    Adds afterAll to e2e testing

--- client/src/components/UserInput/UserInput.js
+++ client/src/components/UserInput/UserInput.js
@@ -3,7 +3,7 @@ import { h } from 'preact';
 import classes from './UserInput.css';
 
 const userInput = ( props ) => (
-  <div>
+  <div id='UserInput'>
     <form onSubmit={props.submitted} className={classes.UserInput}>
       <h1>Find a charging point for your electric vehicle</h1>
       <p>Enter your postcode to begin</p>
--- client/tests/End2EndTesting/loadingPage.test.js
+++ client/tests/End2EndTesting/loadingPage.test.js
@@ -3,20 +3,24 @@ const puppeteer = require('puppeteer');
 let browser;
 let page;
 
-  describe('loading page', () => {
+describe('loading page', () => {
 
-    beforeAll(async () => {
-      browser = await puppeteer.launch({
-        headless: true
-      });
-      page = await browser.newPage();
-      await page.goto('http://localhost:8080');
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true
     });
+    page = await browser.newPage();
+    await page.goto('http://localhost:8080');
+  });
 
-    test('Page loads input and map', async () => {
-      await page.waitForSelector('header');
-      const html =  await page.$eval('#title', e => e.innerHTML);
-      expect(html).toBe('plugIn');
-      browser.close();
-    });
+  afterAll(() => {
+    browser.close()
   })
+
+  test('Page loads title', async () => {
+    await page.waitForSelector('header');
+    const html =  await page.$eval('#title', e => e.innerHTML);
+    expect(html).toBe('plugIn');
+  });
+
+})

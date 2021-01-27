e208ae028144d5e09dbe743b3434d05ecc0a9750
CRTW26 <worthington.connor@gmail.com>
2021-01-27 12:19:59 +0000

    Full testing of input component

--- client/tests/UserInput.test.js
+++ client/tests/UserInput.test.js
@@ -4,8 +4,25 @@ import UserInput from '../src/components/UserInput/UserInput';
 import { shallow } from 'enzyme';
 
 describe('Test of UserInput', () => {
+
 	test('Title of form is rendered', () => {
 		const context = shallow(<UserInput />);
 		expect(context.find('h1').text()).toBe('Find a charging point for your electric vehicle');
-	});
+  });
+
+  test('Prompt for postcode is rendered', () => {
+		const context = shallow(<UserInput />);
+		expect(context.find('p').text()).toBe('Enter your postcode to begin');
+  });
+
+  test('Input is rendered', () => {
+		const context = shallow(<UserInput />);
+		expect(context.find('input'));
+  });
+
+  test('Button is rendered', () => {
+		const context = shallow(<UserInput />);
+		expect(context.find('button').text()).toBe('Search');
+  });
+
 });

ea4ad71a074660a59dc503cc4f5da1352ea0f34c
CRTW26 <worthington.connor@gmail.com>
2021-01-28 11:12:33 +0000

    updates api call to require key

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const cors = require('cors');
+
 
 app.use(cors({
   origin: "http://localhost:8080",
@@ -12,7 +15,6 @@ app.use(cors({
 app.listen(3000, () => console.log('Active on port 3000'));
 
 app.get('/api/location/:postcode', async (request, response) => {
-// app.get('/api/location/', async (request, response) => {
   console.log(request.params)
   const postcode = request.params.postcode
   const locationData = await fetch(`https://api.postcodes.io/postcodes/${postcode}`)
@@ -24,9 +26,8 @@ app.get('/api/location/:postcode', async (request, response) => {
 app.get('/api/stations/:lat/:lng', async (request, response) => {
   const lat = request.params.lat;
   const lng = request.params.lng;
-  console.log(lat)
-  console.log(lng)
-  const stationData = await fetch(`https://api.openchargemap.io/v3/poi/?output=json&countrycode=GB&maxresults=20&compact=true&verbose=false&latitude=${lat}&longitude=${lng}&distance=30&distanceunit=km&opendata=true`);
+  const key = process.env.API_key;
+  const stationData = await fetch(`https://api.openchargemap.io/v3/poi/?output=json&countrycode=GB&maxresults=20&compact=true&verbose=false&latitude=${lat}&longitude=${lng}&distance=30&distanceunit=km&opendata=true&key=${key}`);
   const stationResponse = await stationData.json();
   response.json(stationResponse);
 });

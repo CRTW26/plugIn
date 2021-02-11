040d751285f98008faa7b6c35416bb3ec1e92051
CRTW26 <worthington.connor@gmail.com>
2021-02-11 14:37:20 +0000

    adds about page

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,9 +1,14 @@
 import { h } from 'preact';
+import { Link } from 'preact-router/match';
 import style from './style.css';
 
 const Header = (props) => (
 	<header class={style.header}>
 		<a onClick={() => window.location.reload(false)}><h1 id='title'>plugIn</h1></a>
+        <nav>
+        <Link activeClassName={style.active} href="/">Home</Link>
+        <Link activeClassName={style.active} href="/about">About</Link>
+        </nav>
 	</header>
 );
 
---
+++ client/src/routes/about/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import classes from './style.css';
+
+const About = () => (
+	<div class={classes.App}>
+		<h1>plugIn</h1>
+		<p>This app has been designed to help users locate charging stations for electric vehicles.</p>
+	</div>
+);
+
+export default About;

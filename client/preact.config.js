56288129334ada4fcd13c3a2bcae8ffa2a4a9161
CRTW26 <worthington.connor@gmail.com>
2021-02-11 13:35:44 +0000

    preact config

--- client/preact.config.js
+++ client/preact.config.js
@@ -1,12 +1,12 @@
-module.exports = function(config) {
-  config.devServer.proxy = [
-    {
-      // proxy requests matching a pattern:
-      path: '/api/**',
+// module.exports = function(config) {
+//   config.devServer.proxy = [
+//     {
+//       // proxy requests matching a pattern:
+//       path: '/api/**',
 
-      // where to proxy to:
-      target: 'http://localhost:3000',
+//       // where to proxy to:
+//       target: 'http://localhost:3000',
 
-    }
-  ];
-};
+//     }
+//   ];
+// };

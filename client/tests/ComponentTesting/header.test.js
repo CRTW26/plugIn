84f0d022d41c924a28b2bd384a9cedb91c5d38f2
CRTW26 <worthington.connor@gmail.com>
2021-02-11 15:34:01 +0000

    Adds test for number of nav bar items

--- client/tests/ComponentTesting/header.test.js
+++ client/tests/ComponentTesting/header.test.js
@@ -7,5 +7,6 @@ describe('Initial Test of the Header', () => {
 	test('Header renders 3 nav items', () => {
 		const context = shallow(<Header />);
 		expect(context.find('h1').text()).toBe('plugIn');
+        expect(context.find('Link').length).toBe(2);
 	});
 });
